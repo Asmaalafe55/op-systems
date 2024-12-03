09646c76d2232b160a27a6ff57ba030ac867f211
Asmaalafe55 <asmaalafe55@gmail.com>
2024-12-03 15:20:51 +0200

    adding the main scripts

--- task3/task3.sh
+++ task3/task3.sh
@@ -1,2 +1,8 @@
+echo "The name of the directory I am currently in: "
+pwd
+echo "Which bash I am ?"
+which bash
+echo "The files in the directory I am currently in: "
+ls
 echo "In how much lines does small_airports appears in file airports: "
 grep "small_airport" airports.csv | wc -l
